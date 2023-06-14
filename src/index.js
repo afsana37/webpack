6d775925cf1c93463c1e5b012a4f8656359e7447
Afsana Abida <afsanamist37@gmail.com>
2023-06-14 17:36:56 +0200

    yaml toml and json5 data added

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
+ import './style.css';
+ import Icon from './icon.png';
+ import Data from './data.xml';
+ import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
 
-function component() {
-  const element = document.createElement('div');
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
-  element.appendChild(myIcon);
+ function component() {
+   const element = document.createElement('div');
 
- console.log(Data);
- console.log(Notes);
+   // Lodash, now imported by this script
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   element.classList.add('hello');
 
-  return element;
-}
+   // Add the image to our existing div.
+   const myIcon = new Image();
+   myIcon.src = Icon;
 
-document.body.appendChild(component());
+   element.appendChild(myIcon);
+
+   console.log(Data);
+   console.log(Notes);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
