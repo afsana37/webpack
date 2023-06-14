b494ee0ee28c5e02b4748ca0b2a89c207b95322e
Afsana Abida <afsanamist37@gmail.com>
2023-06-14 17:32:16 +0200

    data created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import _ from 'lodash';
- import './style.css';
+import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
- function component() {
-   const element = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
 
-   // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   // Add the image to our existing div.
   const myIcon = new Image();
@@ -15,7 +17,10 @@ import Icon from './icon.png';
 
   element.appendChild(myIcon);
 
-   return element;
- }
+ console.log(Data);
+ console.log(Notes);
 
- document.body.appendChild(component());
+  return element;
+}
+
+document.body.appendChild(component());
